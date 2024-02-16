7d08c5f91c8817e5ed83a7e62039eb5f6db6a69f
DXL44 <28364459+DXL44@users.noreply.github.com>
2024-02-16 00:27:28 -0500

    Automated page metadata and formatting

--- index.js
+++ index.js
@@ -1,119 +1,184 @@
-
 const messagesInput = require('./messages.json');
+const userData = require('./users.json');
+
 // keepin it constant
 const dotenv = require("dotenv")
 require('dotenv').config()
-const { Client } = require("@notionhq/client")
+const {
+    Client
+} = require("@notionhq/client")
+const {
+    markdownToBlocks,
+    markdownToRichText
+} = require('@tryfabric/martian');
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+
+const notion = new Client({
+    auth: process.env.NOTION_KEY
+})
 const databaseId = process.env.NOTION_DATABASE_ID
 
 //variable for the page content
 
-var pageContent = [
-  {
-  "object": "block",
-  "type": "callout",
-  "callout": {
-    "rich_text": 
-    [
-      { 
-        "type": "text", 
-        "annotations": {
-          "bold": true,
-          "underline": true,
-        },
-        "text": { 
-          "content": "Placeholder"
-        } 
-      },
-      { 
-        "type": "text", 
-        "text": { 
-          "content": "\n" + "Placeholder"
-        } 
-      }
-    ],
-    "color":"default",
-  },
-}
-]
-//so here's the part where we convert each message into a block
-for (i=0; i< messagesInput.messages.length && i < 95; i++){
-  console.log("Adding message " + i);
-  messageSelected = messagesInput.messages[i]
-  messageContent = {
+var pageContent = [{
     "object": "block",
     "type": "callout",
     "callout": {
-      "rich_text": 
-      [
-        { 
-          "type": "text", 
-          "annotations": {
+        "rich_text": [{
+                "type": "text",
+                "annotations": {
+                    "bold": true,
+                    "underline": true,
+                },
+                "text": {
+                    "content": "Placeholder"
+                }
+            },
+            {
+                "type": "text",
+                "text": {
+                    "content": "\n" + "Placeholder"
+                }
+            }
+        ],
+        "color": "default",
+    },
+}]
+// Create an array of users in the given selection of messages.
+const chatUsers = []
+
+console.log("Building message callouts...")
+/* BUILD MESSAGES INTO BLOCKS */
+for (i = 0; i < messagesInput.messages.length && i < 95; i++) {
+
+    //console.log("Adding message " + i + "...");
+    messageSelected = messagesInput.messages[i]
+
+    // Find out of this is a new user
+    var thisUser = userData.find((element) => element.name == messageSelected.author.name)
+    if (!thisUser) {
+        //console.log(`WARNING: User ${messageSelected.author.name} not found in users.json!`)
+        thisUser = {
+            "name": messageSelected.author.name,
+            "color": "default",
+            "author": "Unknown Author"
+        }
+    }
+    if (!chatUsers.find((element) => element.name == thisUser.name))
+    {
+      //console.log(`User logged: ${thisUser.name}`)
+      chatUsers.push(thisUser);
+    }
+
+    //console.log("Building rich text from markdown...")
+    messageRich = markdownToRichText(messageSelected.content)
+    // add actual message content now :)
+    messageRich.unshift({
+        "type": "text",
+        "annotations": {
             "bold": true,
             "underline": true,
-          },
-          "text": { 
-            "content": messageSelected.author.name
-          } 
         },
-        { 
-          "type": "text", 
-          "text": { 
-            "content": "\n" + messageSelected.content
-          } 
+        "text": {
+            "content": messageSelected.author.name
         }
-      ],
-      "icon": {
-        "external":{
-          "url":messageSelected.author.avatarUrl,
+    }, {
+        "type": "text",
+        "text": {
+            "content": "\n"
         }
-      },
-      "color":"default",
-    },
-  };
-  pageContent.push(messageContent)
+    }, )
+    messageContent = {
+        "object": "block",
+        "type": "callout",
+        "callout": {
+            "rich_text": messageRich,
+            "icon": {
+                "external": {
+                    "url": messageSelected.author.avatarUrl,
+                }
+            },
+            "color": thisUser.color,
+        },
+    };
+    pageContent.push(messageContent)
+    process.stdout.clearLine(0);
+
 }
-pageName = "GRAHHHHHH"
-console.log(`Page name set to ${pageName}`)
-// remove placeholder 
+process.stdout.write("\n");
+/* SET PAGE NAME */
+pageName = "Untitled Archive"
+//console.log(`Page name set to ${pageName}`)
+// remove placeholder content 
 pageContent.shift();
+// its time for page metadata!
 
-//if this isnt a whole channel then show the date range in the title
+timestamp = messagesInput.messages[0].timestamp
+timestamp = timestamp.substr(0,10)
 
-/*
-if (messageContent.dateRange.after == null || messageContent.dateRange.before == null) {
-   
-} else {
-  pageName = `${messagecontent.name}|${messagecontent.dateRange.before}-${messagecontent.dateRange.after}`
-} */
-
-createNotionPage()
-
-async function createNotionPage() {
-  const data = {
-    "parent": {
-      "type": "database_id",
-      "database_id": process.env.NOTION_DATABASE_ID
-    }, 
-    "properties": {
-      "Name": {
-        "title": [
-          {
-            "text": {
-              "content": pageName
-            }
-          }
-        ]
-      },
-    },
-    "children": pageContent
-
-  }
-    console.log(`Initiating shenanigans...`)
-    const response = await notion.pages.create( data )
-    console.log(`Created page at ID ` + response.id)
-  
+console.log(`--PAGE SUMMARY--
+START: ${timestamp}
+URL: https://discord.com/channels/${messagesInput.guild.id}/${messagesInput.channel.id}/${messagesInput.messages[0].id}
+MESSAGES: ${messagesInput.messageCount}`)
+console.log("CHARACTERS/AUTHORS:")
+charactersList = [];
+authorsList = [];
+for (i = 0; i < chatUsers.length; i++)
+{
+if (!(chatUsers[i].author == "Unknown Author"))
+{
+  if (!chatUsers[i].ignore) { charactersList.push({"name":chatUsers[i].name}); };
+  authorsList.push({"name":chatUsers[i].author});
+  process.stdout.write(`${chatUsers[i].name} (${chatUsers[i].author}), `);
 }
-    
+}
+process.stdout.write("\n");
+
+
+
+
+//console.log(timestamp)
+createNotionPage(messagesInput, charactersList, authorsList, timestamp)
+
+async function createNotionPage(messageJSON, charactersJSON, authorsJSON, time) {
+    const data = {
+        "parent": {
+            "type": "database_id",
+            "database_id": process.env.NOTION_DATABASE_ID
+        },
+        "properties": {
+            "Name": {
+                "title": [{
+                    "text": {
+                        "content": pageName
+                    }
+                }]
+            },
+            "Characters": {
+              "multi_select": charactersJSON
+            },
+            "Writers": {
+              "multi_select": authorsJSON
+            },
+            "URL": {
+              "url":`https://discord.com/channels/${messageJSON.guild.id}/${messageJSON.channel.id}/${messageJSON.messages[0].id}`
+            },
+            "Location": {
+              "select": {
+                "name": `#${messageJSON.channel.name}`
+              }
+            },
+            "Date": {
+              "date": {
+                "start": time
+              }
+            }
+        },
+        "children": pageContent
+
+    }
+    console.log(`Page fully assembled! Sending to Notion database...`)
+    const response = await notion.pages.create(data)
+    console.log(`ALL DONE!\nPage created at ID: ` + response.id)
+
+}
